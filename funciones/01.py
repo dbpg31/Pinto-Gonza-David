833f831b8d79901a93e225f72c79cc82211306c1
david <dbpintogonza@gmail.com>
2023-08-24 15:04:57 -0500

    01

--- funciones/01.py
+++ funciones/01.py
@@ -1,5 +1,11 @@
 import os
 os.system("cls")
 
-def multiplica(num1,num2): 
+num1 = int(input("multiplicador : "))
+num2 = int(input("multiplicando : "))
+
+
+def multiplica(num1,num2):
+    producto = 0
+    if     
 
---
+++ funciones/02.py
@@ -0,0 +1,13 @@
+import os
+os.system("cls")
+
+# numero=int(input("Nùmero: "))
+
+def cuenta_atras(numero):
+    numero-=1
+    if numero > 0 :
+        print(numero)
+        cuenta_atras(numero)
+
+cuenta_atras(10)
+
