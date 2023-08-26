3e8a5fa1fcb6698b7dd592fa9c0016f3f48d05b3
david pinto <dbpintogonza@gmail.com>
2023-08-26 01:26:11 -0500

    02

--- condicionales/13.py
+++ condicionales/13.py
@@ -7,6 +7,6 @@ unidad = numero % 10
 decena = int((numero % 100)/10)
 centena = int((numero % 1000)/100)
 
-if unidad == decena -1 : print("consecutivo")
-
-
+if unidad == decena - 1 and decena == centena - 1 : print(numero ,": es consecutivo descendente")
+elif unidad == decena + 1 and decena == centena + 1 : print(numero,": es consecutivo ascendente")
+else : print(numero,": no es consecutivo")
