d7ca9042f1dca99a38b6a501cdb67a98448cdb7c
david pinto <dbpintogonza@gmail.com>
2023-09-17 23:31:36 -0500

    05

--- condicionales/19.py
+++ condicionales/19.py
@@ -7,7 +7,9 @@ edad = int (input("edad : "))
 if sexo == "femenino" and edad <=23 : categoria = "FA"
 elif sexo == "femenino" and edad > 23 : categoria = "FB"
 
-if sexo == "masculino" and edad <=23 : categoria = "MA"
-elif sexo == "masculino" and edad > 23 : categoria = "MB"
+if sexo == "masculino" and edad <=25 : categoria = "MA"
+elif sexo == "masculino" and edad > 25 : categoria = "MB"
 
 print(F"categoria del postulate : {categoria}")
+
+
